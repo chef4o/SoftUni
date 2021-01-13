d9fbe808409d841ad5a67497ce869616968a1790
Home <sihristov@hotmail.com>
2021-01-13 20:26:04 +0200

    Adding C# basics lectures tasks - Conditional statements

---
+++ C# Basics/Conditions/PasswordCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordChack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string secret = "s3cr3t!P@ssw0rd";
+
+            if (password == secret)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ C# Basics/Conditions/ToyShop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Transactions;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double TripPrice = double.Parse(Console.ReadLine());
+            int puzzleCount = int.Parse(Console.ReadLine());
+            int dollCount = int.Parse(Console.ReadLine());
+            int bearCount = int.Parse(Console.ReadLine());
+            int minionCount = int.Parse(Console.ReadLine());
+            int truckCount = int.Parse(Console.ReadLine());
+
+            double puzzlePrice = puzzleCount * 2.6;
+            double dollPrice = dollCount * 3;
+            double bearPrice = bearCount * 4.1;
+            double minionPrice = minionCount * 8.2;
+            double truckPrice = truckCount * 2;
+
+            double totalCount = puzzleCount + dollCount + bearCount + minionCount + truckCount;
+            double totalPrice = puzzlePrice + dollPrice + bearPrice + minionPrice + truckPrice;
+
+            if (totalCount >= 50)
+            {
+                totalPrice = totalPrice - (totalPrice * 25 / 100);
+            }
+
+            double profit = totalPrice - (totalPrice * 10 / 100);
+
+            if (profit >= TripPrice)
+            {
+                double leftover = profit - TripPrice;
+                Console.WriteLine($"Yes! {leftover:f2} lv left.");
+            }
+            else
+            {
+                double leftover = TripPrice - profit;
+                Console.WriteLine($"Not enough money! {leftover:f2} lv needed.");
+            }
+        }
+    }
+}

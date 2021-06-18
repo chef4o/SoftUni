96096b564db91f3e80ba08bcc4bb84483637608e
Stefan Hristov <sihristov@hotmail.com>
2021-06-18 18:28:24 +0300

    Adding exercises on Generics

---
+++ Java Advanced/GenericsEx/BoxOfInts/Box.java
@@ -0,0 +1,17 @@
+package GenericsEx.BoxOfInts;
+
+public class Box<T> {
+
+    private T value;
+
+    public Box(T value) {
+
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("%s: %s", this.value.getClass().getName(), value);
+    }
+}
